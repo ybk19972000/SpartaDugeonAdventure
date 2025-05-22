e3dbabc52ba9c89bf8c9ae96db90061eaf1cbcbd
윤병길 <50022221+ybk19972000@users.noreply.github.com>
2025-05-22 19:10:30 +0900

    Feat,Fix:아이템,블록 프리팹 생성
    아이템과 블록 프리팹 생성 및 수정

---
+++ Assets/Scripts/ItemObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour,IInteractable
+{
+    public ItemData data;
+
+    public string GetInteractPrompt()
+    {
+        string str = $"{data.displayName}\n{data.description}";
+        return str;
+    }
+
+    public void OnInteract()
+    {
+        CharacterManager.Instance.Player.itemData = data;
+        CharacterManager.Instance.Player.addItem?.Invoke();
+        Destroy(gameObject);
+    }
+}
