4da69418690e8b576fb10b707375b4620afa3622
윤병길 <50022221+ybk19972000@users.noreply.github.com>
2025-05-23 15:58:53 +0900

    fix,feat:자잘한 코드 수정, 빌드 추가

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
+    public Button gameStart;
+    public Button gameExit;
 
+    public void Start()
+    {
+        
+    }
+
+    public void UpdateScore(float score)
+    {
+        scoreText.text = score.ToString();
+    }
 }
